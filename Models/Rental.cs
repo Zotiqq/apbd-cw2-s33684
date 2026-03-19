7d07b89ce1a25aa6204b70ea751ba7c4838f6150
Zotiqq <haxplayer11@gmail.com>
2026-03-19 09:39:19 +0100

    Create class Rental

---
+++ Models/Rental.cs
@@ -0,0 +1,20 @@
+﻿namespace apbd_cw2_s33684.Models;
+
+public class Rental
+{
+    public User User { get; set; }
+    public Equipment Item { get; set; }
+    public DateTime DateRented { get; set; }
+    public DateTime Deadline { get; set; }
+    public DateTime? DateReturned { get; set; }
+    public decimal Penalty { get; set; }
+
+    public Rental(User user, Equipment item, DateTime deadline)
+    {
+        User = user;
+        Item = item;
+        DateRented = DateTime.Now;
+        Deadline = deadline;
+        Penalty = 0;
+    }
+}
