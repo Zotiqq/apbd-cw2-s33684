19a0f25583f4cca0efc54bc78649f519b04f8c1d
Zotiqq <haxplayer11@gmail.com>
2026-03-19 09:32:12 +0100

    Create class Camera

---
+++ Models/Camera.cs
@@ -0,0 +1,13 @@
+﻿namespace apbd_cw2_s33684.Models;
+
+public class Camera : Equipment
+{
+    public string SensorType { get; set; }
+    public bool CanRecord4K { get; set; }
+
+    public Camera(int id, string name, string sensorType, bool canRecord4K) : base(id, name)
+    {
+        SensorType = sensorType;
+        CanRecord4K = canRecord4K;
+    }
+}
