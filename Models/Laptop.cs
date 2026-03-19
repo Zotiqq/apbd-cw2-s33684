45079adcafadb150f5fff3f798a8df6d80418003
Zotiqq <haxplayer11@gmail.com>
2026-03-19 09:28:13 +0100

    Create class Laptop

---
+++ Models/Laptop.cs
@@ -0,0 +1,13 @@
+﻿namespace apbd_cw2_s33684.Models;
+
+public class Laptop : Equipment
+{
+    public string Processor { get; set; }
+    public int Ram { get; set; }
+
+    public Laptop(int id, string name, string processor, int ram) : base(id, name)
+    {
+        Processor = processor;
+        Ram = ram;
+    }
+}
