4ad4e55b3bfe6fc61d0f5bc0d790c53ae4cd1ff7
Zotiqq <haxplayer11@gmail.com>
2026-03-19 09:30:39 +0100

    Create class Projector

---
+++ Models/Projector.cs
@@ -0,0 +1,13 @@
+﻿namespace apbd_cw2_s33684.Models;
+
+public class Projector : Equipment
+{
+    public string Resolution { get; set; }
+    public string ConnectorType { get; set; }
+
+    public Projector(int id, string name, string resolution, string connectorType) : base(id, name)
+    {
+        Resolution = resolution;
+        ConnectorType = connectorType;
+    }
+}
