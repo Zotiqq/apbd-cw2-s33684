c72f0e0640614bc293d707e156b64a8ee7cbd528
Zotiqq <haxplayer11@gmail.com>
2026-03-19 09:22:48 +0100

    Create class Equipment

---
+++ Models/Equipment.cs
@@ -0,0 +1,14 @@
+﻿namespace apbd_cw2_s33684.Models;
+
+public class Equipment
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public bool IsAvailable { get; set; } = true;
+
+    public Equipment(int id, string name)
+    {
+        Id = id;
+        Name = name;
+    }
+}
