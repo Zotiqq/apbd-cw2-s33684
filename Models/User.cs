2ee6186111f26dd4239532a70255b187c9b4890e
Zotiqq <haxplayer11@gmail.com>
2026-03-19 09:20:11 +0100

    Create class User

---
+++ Models/User.cs
@@ -0,0 +1,14 @@
+﻿namespace apbd_cw2_s33684.Models;
+
+public class User
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public string Role { get; set; }
+
+    public User(int id, string name, string surname, string role)
+    {
+        Id = id; Name = name; Surname = surname; Role = role;
+    }
+}
